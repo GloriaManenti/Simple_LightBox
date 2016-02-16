0b407a097a55faffdf4e2c6c965be029b2bd32cf
behzad najafi <behzad najafi>
2016-02-16 14:13:34 +0100

    adding the caption

--- js/app.js
+++ js/app.js
@@ -2,12 +2,16 @@
 // Capture the click event on a link to ann image
 var $overlay= $('<div id="overlay"></div>');
 var $image = $("<img>");
+var $caption=$("<p></p>")
 $overlay.append($image);
+$overlay.append($caption);
 $("body").append($overlay);
 $("#imageGallery a").click(function(event){
 	event.preventDefault();
-var imageLocation = $(this).attr("href")
+var imageLocation = $(this).attr("href");
+var imageCaption= $(this).children("img").attr("alt");
 $image.attr("src",imageLocation);
+$caption.text(imageCaption);
 $overlay.show();
 });
 	//show the overaly
